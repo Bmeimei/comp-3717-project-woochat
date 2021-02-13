0df8bb917c5d010b9f77149ca8e9362f17f8b867
Geun Uoo Kim <gonukimxxiv@gmail.com>
2021-02-12 16:36:57 -0800

    implemented fragments to the nav bar

--- app/src/main/java/com/example/woochat/MainActivity.java
+++ app/src/main/java/com/example/woochat/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.woochat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.example.woochat.fragments.ChatFragment;
+import com.example.woochat.fragments.FriendsFragment;
+import com.example.woochat.fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +23,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        Fragment friendsFragment = new FriendsFragment();
+        Fragment chatFragment = new ChatFragment();
+        Fragment settingsFragment = new SettingsFragment();
+
+        makeCurrentFragment(friendsFragment);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.ic_friends:
+                       makeCurrentFragment(friendsFragment);
+                    case R.id.ic_chat:
+                        makeCurrentFragment(chatFragment);
+                    case R.id.ic_settings:
+                        makeCurrentFragment(settingsFragment);
+                }
+                return true;
+            }
+        });
+
     }
+
+    private void makeCurrentFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fl_wrapper, fragment);
+        transaction.commit();
+    }
+
 }
