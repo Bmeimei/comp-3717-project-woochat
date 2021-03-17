28efcd7f8946524f17ff2d3486faaa3ff75d078c
luke <skystar505836915@gmail.com>
2021-03-17 15:46:46 -0700

    first commit

--- app/src/main/java/com/example/woochat/Landing.java
+++ app/src/main/java/com/example/woochat/Landing.java
@@ -2,13 +2,35 @@ package com.example.woochat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Landing extends AppCompatActivity {
+
+    Button loginButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(v -> goToLoginPage());
+        signUpButton.setOnClickListener(v -> goToSignUpPage());
+    }
+
+    public void goToLoginPage() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void goToSignUpPage() {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/woochat/Login.java
+++ app/src/main/java/com/example/woochat/Login.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
