22c979b2e7abb490c369927e28f9666dc6e2a870
luke <skystar505836915@gmail.com>
2021-03-18 19:28:20 -0700

    add message id for message

--- app/src/main/java/com/example/woochat/Message.java
+++ app/src/main/java/com/example/woochat/Message.java
@@ -3,12 +3,18 @@ package com.example.woochat;
 import java.util.Date;
 
 public class Message {
+    final String messageId;
     final String senderId;
     final String receiverId;
     final String messageContent;
     final Date time;
 
-    public Message(String senderId, String receiverId, String messageContent, Date time) {
+    public Message(String messageId,
+                   String senderId,
+                   String receiverId,
+                   String messageContent,
+                   Date time) {
+        this.messageId = messageId;
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.messageContent = messageContent;
